3cbef0bd424f831c4c3b051675987cb2d2ae6554
Cong Nguyen <nguyencongkhai1407@gmail.com>
2022-03-20 13:26:32 -0500

    drawing rectangle on the detected eyes on top of the detected face

--- main.py
+++ main.py
@@ -12,7 +12,11 @@ while True:
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 5)
-
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi_gray, 1.3, 5)
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 5)
 
     cv2.imshow('frame', frame)
 

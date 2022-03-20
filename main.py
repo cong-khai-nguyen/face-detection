591a91974348fad6c066348a546abbee5cf4d203
Cong Nguyen <nguyencongkhai1407@gmail.com>
2022-03-20 13:25:41 -0500

    drawing rectangle on the detected face

--- main.py
+++ main.py
@@ -7,7 +7,15 @@ eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml
 
 while True:
     ret, frame = cap.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 5)
+
+
     cv2.imshow('frame', frame)
+
     if cv2.waitKey(1) == ord('q'):
         break
 
